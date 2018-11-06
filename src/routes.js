b6f67775c9f92e1e1ee5e46a667b8ff83824e691
Junior <Junior@DESKTOP-14GQFD1>
2018-11-06 18:11:23 -0200

    api created

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const routes = require('express')
+                       .Router();
+
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store)
+
+routes.post('/likes/:id', LikeController.store);
+
+module.exports = routes;                      
